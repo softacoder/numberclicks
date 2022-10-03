3a47a9e56a040761468b56c1e65f9a575cdefd19
softacoder <113185323+softacoder@users.noreply.github.com>
2022-10-03 18:02:56 +0100

    Add files via upload

---
+++ numberclicks.js
@@ -0,0 +1,11 @@
+var click_count;
+
+
+click_count = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
+  event.target.innerText = click_count;
+
+});
